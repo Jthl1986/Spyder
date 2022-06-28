47cef585d3ee5a00f98e385d009b5e4333f0a195
Jorge Santacecilia <jorgesantacecilia@gmail.com>
2022-06-28 15:22:40 -0300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,15 @@
+# -import streamlit as st
+import plotly.graph_objects as go
+import numpy as np
+st.set_page_config(
+    page_title="Financial Planning Calculator")
+
+st.title("Financial Planning Calculator")
+st.header("**Monthly Income**")
+st.subheader("Salary")*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
