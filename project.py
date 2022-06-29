8e81a8021c6f2c59e2e3957abb4115b1510c120d
Jorge Santacecilia <jorgesantacecilia@gmail.com>
2022-06-28 21:26:10 -0300

    Update project.py

--- project.py
+++ project.py
@@ -7,8 +7,8 @@ st.title("Valuación de hacienda")
 
 coltipo, colcantidad, colpeso = st.columns(3)
 with coltipo:
-    tipo = st.text_input("Ingrese categoría de hacienda: ")
+    tipo = st.text_input('ingrese tipo de hacienda: \n1 - Ternero\n2 - Novillito\n3 - Ternera\n4 - Vaquillona\n5 - Vaca\n')
 with colcantidad:
-    cantidad = st.number_input("Ingrese cantidad de cabezas: ", min_value=0.0, format='%f')
+    cantidad = st.number_input("Ingrese cantidad de cabezas: ", min_value=0.0)
 with colpeso:
-    peso = st.number_input("Ingrese peso: ", min_value=0.0, format='%f')
+    peso = st.number_input("Ingrese peso: ", min_value=0.0)
