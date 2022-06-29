d2415f89693d50bb77b51d59841678acaf597cab
Jorge Santacecilia <jorgesantacecilia@gmail.com>
2022-06-28 21:30:45 -0300

    Update project.py

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ st.title("Valuación de hacienda")
 
 coltipo, colcantidad, colpeso = st.columns(3)
 with coltipo:
-    tipo = st.text_input('ingrese tipo de hacienda: \n1 - Ternero\n2 - Novillito\n3 - Ternera\n4 - Vaquillona\n5 - Vaca\n')
+    tipo = st.selectbox('ingrese tipo de hacienda: ', [" Ternero", "Novillito", "Ternera", "Vaquillona", "Vaca"])
 with colcantidad:
     cantidad = st.number_input("Ingrese cantidad de cabezas: ", min_value=0.0)
 with colpeso:
